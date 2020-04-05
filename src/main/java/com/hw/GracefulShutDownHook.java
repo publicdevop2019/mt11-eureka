359948c1895f7d66574b551edf117ad33944756b
publicdevop2019 <publicdevop2019hw@gmail.com>
2020-04-04 21:11:30 -0400

    remove poc

--- src/main/java/com/hw/GracefulShutDownHook.java
+++ src/main/java/com/hw/GracefulShutDownHook.java
@@ -2,17 +2,9 @@ package com.hw;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.logging.log4j.LogManager;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.commons.util.InetUtils;
-import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PreDestroy;
-import java.net.NetworkInterface;
-import java.net.SocketException;
-import java.util.Enumeration;
-import java.util.Iterator;
 
 @Configuration
 @Slf4j
@@ -21,28 +13,5 @@ public class GracefulShutDownHook {
     public void onExit() {
         log.info("Closing application..");
         LogManager.shutdown();
-    }
-
-    @Autowired
-    InetUtils inetUtils;
-
-    @Bean
-    public void eurekaInstanceConfig() {
-        EurekaInstanceConfigBean config = new EurekaInstanceConfigBean(inetUtils);
-        try {
-            Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
-            Iterator<NetworkInterface> networkInterfaceIterator = networkInterfaces.asIterator();
-            networkInterfaceIterator.forEachRemaining(e -> {
-                e.getInterfaceAddresses().stream().forEach(el -> {
-                    log.info("NetworkInterface.getNetworkInterfaces " + el.getAddress().getHostAddress());
-                    log.info("NetworkInterface.getNetworkInterfaces " + el.getAddress().getHostName());
-                });
-            });
-        } catch (SocketException e) {
-            e.printStackTrace();
-        }
-        log.info("getHostAddress " + inetUtils.findFirstNonLoopbackAddress().getHostAddress());
-        log.info("getIpAddress " + inetUtils.findFirstNonLoopbackHostInfo().getIpAddress());
-        log.info("getHostname " + inetUtils.findFirstNonLoopbackHostInfo().getHostname());
     }
 }
